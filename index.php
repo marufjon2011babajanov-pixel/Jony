0df39993ec83af94e0bc9aced55f8d218a6a4c16
Jony <github.com/marufjon2011babajanov-pixel>
2025-11-04 15:56:56 +0500

    lessons

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
-$user=["name"=>"Jony","age"=>14,"login"=>"Jony_2011"];
-$user["password"]="Jony_M";
-foreach($user as $a=>$b){
-    echo $a."=".$b."<br>";
+$a=[10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10];
+$b=sort($a);
+for($i=0; $i<count($a);$i++){
+    echo $a[$i]."<br>";
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: yellow ;
+        }
+     h1{
+   color:red;
+   border:10px solid green;
+   border-radius:50px;
+   text-align:center;-
+   padding: 20px ;
+   background-color:blue;
+     }
+    </style>
+</head>
+<body>
+<h1><?=$i?></h1>
+<hr>
+</body>
+</html>               
